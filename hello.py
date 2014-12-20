4c95d8c780af7eae2942d541b4f4cc1bd7e2eef8
enderzombies4 <brandongoose1@gmail.com>
2014-12-19 19:10:59 -0500

    First Program! :)

---
+++ hello.py
@@ -0,0 +1,14 @@
+import random 
+
+
+answers = ['Yes','No','My Sources Say No','For Sure']
+
+print('''
+Welcome To The Magic 8 Ball!
+Enter Your Question Below:
+''')
+
+while True:
+    question = input('> ')
+    answer = random.choice(answers)
+    print(answer)
