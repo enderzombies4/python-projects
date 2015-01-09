774ecb64607ba56ade67a18e9bc3175206917ff2
enderzombies4 <brandongoose1@gmail.com>
2015-01-09 18:24:57 -0500

    The Quiz of Utterly Doom!
    Take the four question quiz that is crazy! #ImpossibleQuizByBrandon

---
+++ Python_QuizGame.py
@@ -0,0 +1,41 @@
+print('''
+In Clash of
+clans how many
+levels of town
+halls are there?
+''')
+answer = input(' >')
+if int(answer) == 10:
+    print('Correct!')
+else:
+    print('Wrong!')
+print('''
+What year was
+Minecraft 1.8
+released?
+''')
+answer = input(' >')
+if int(answer) == 2014:
+    print('Correct!')
+else:
+    print('Wrong!')
+print('''
+What is my best
+friend's name?
+''')
+answer = input(' >')
+if 'Riley' in answer:
+    print('Correct!')
+else:
+    print('Wrong!')
+print('''
+It requires
+2 sticks and how many
+diamonds to make a
+diamond hoe in Minecraft?
+''')
+answer = input(' >')
+if int(answer) == 2:
+    print('Correct!')
+else:
+    print('Wrong!')
